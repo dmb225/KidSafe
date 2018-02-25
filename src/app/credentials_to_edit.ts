aa4f06985f3572834a21915ded8df2ef2f35d9c1
dmb <daomalick225@gmail.com>
2018-02-25 11:37:55 +0100

    Updating README.md

---
+++ src/app/credentials_to_edit.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "to_edit",
+  authDomain: "to_edit",
+  databaseURL: "to_edit",
+  projectId: "to_edit",
+  storageBucket: "to_edit",
+  messagingSenderId: "to_edit"
+};
